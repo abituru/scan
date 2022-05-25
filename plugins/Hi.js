6ca3a100cf8bfb9fa396ccf991c0283207fada3d
romlisuhanda <104242110+romlisuhanda@users.noreply.github.com>
2022-05-26 05:41:59 +0700

    Update Hi.js

--- plugins/Hi.js
+++ plugins/Hi.js
@@ -1,16 +1,13 @@
 let fetch = require('node-fetch')
-let handler = async (m, { conn }) => conn.send2ButtonLoc(m.chat, await (await fetch(fla + 'KRIZYN BOTz')).buffer(), `
+let handler = async (m, { conn }) => conn.send2ButtonLoc(m.chat, await (await fetch(fla + 'ZXYUU BOTZ')).buffer(), `
 📮 𝘊𝘈𝘙𝘐 𝘈𝘑𝘈 𝘋𝘐 𝘎𝘐𝘛𝘏𝘜𝘉 𝘖𝘙 𝘠𝘖𝘜 𝘛𝘜𝘉𝘌
-➵͜͡✪ 𝙶𝙸𝚃𝙷𝚄𝙱 :
-github.com/frostkri/krizynv1
-➵͜͡✪ 𝚈𝙾𝚄 𝚃𝚄𝙱𝙴 :
-youtube.com/c/hokenbeusz
-➵͜͡✪ 𝙹𝚊𝚗𝚐𝚊𝚗 𝚕𝚞𝚙𝚊 𝚖𝚊𝚖𝚙𝚒𝚛
-https://lynk.id/kri.com
+➵͜͡✪ 𝙶𝙸𝚃𝙷𝚄𝙱 :-
+➵͜͡✪ 𝚈𝙾𝚄 𝚃𝚄𝙱𝙴 :-
+➵͜͡✪ 𝙹𝚊𝚗𝚐𝚊𝚗 𝚕𝚞𝚙𝚊 𝚖𝚊𝚖𝚙𝚒𝚛 :-
 `.trim(), ' 𝘋𝘰𝘯𝘵 𝘧𝘰𝘳𝘨𝘦𝘵 𝘵𝘰 𝘧𝘰𝘭𝘭𝘰𝘸 𝘮𝘺 𝘢𝘤𝘤𝘰𝘶𝘯𝘵', 'Thanks', '.tqto', 'Menu','.menu')
 handler.help = ['sourcecode']
 -
-`.trim(), '© 𝙆𝙧𝙞𝙯𝙮𝙣×͜×', 'Thanks', '.tqto', 'Back','.menu')
+`.trim(), '© ZxyuuBotz', 'Thanks', '.tqto', 'Back','.menu')
 handler.help = ['sourcecode']
 handler.tags = ['info']
 handler.command = /^(sourcecode|sc|scbot|script|github)$/i
