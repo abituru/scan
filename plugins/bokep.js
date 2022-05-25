04fc6098fd95488635bb1d407552525c1f88ead8
romlisuhanda <104242110+romlisuhanda@users.noreply.github.com>
2022-05-26 05:50:17 +0700

    Update bokep.js

--- plugins/bokep.js
+++ plugins/bokep.js
@@ -1,6 +1,6 @@
 let handler = async (m, { conn }) => {
 if (!db.data.chats[m.chat].nsfw && m.isGroup) throw global.nsfw
-conn.sendFile(m.chat, 'https://yog-apikey.herokuapp.com/api/bokep?apikey=YogGanz', 'asupan.mp4', 'Nih Jngn comly  ya', m, false, { contextInfo: { forwardingScore: 999, isForwarded: true }})
+conn.sendFile(m.chat, 'https://yog-apikey.herokuapp.com/api/bokep?apikey=YogGanz', 'asupan.mp4', 'HADEH COMLY MULU BANG!!', m, false, { contextInfo: { forwardingScore: 999, isForwarded: true }})
 }
 handler.help = ['bkp', 'bokep']
 handler.tags = ['bokep']
